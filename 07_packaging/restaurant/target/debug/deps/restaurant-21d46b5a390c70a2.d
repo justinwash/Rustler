58c8f0595a5c420f4d86986989d5430edac746da
EC2 Default User <ec2-user@ip-172-31-18-12.us-east-2.compute.internal>
2019-07-01 21:10:16 +0000

    packaging

---
+++ 07_packaging/restaurant/target/debug/deps/restaurant-21d46b5a390c70a2.d
@@ -0,0 +1,6 @@
+/home/ec2-user/environment/07_packaging/restaurant/target/debug/deps/librestaurant-21d46b5a390c70a2.rlib: src/lib.rs src/front_of_house.rs
+
+/home/ec2-user/environment/07_packaging/restaurant/target/debug/deps/restaurant-21d46b5a390c70a2.d: src/lib.rs src/front_of_house.rs
+
+src/lib.rs:
+src/front_of_house.rs:
---
+++ 07_packaging/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/ec2-user/environment/07_packaging/restaurant/target/debug/librestaurant.rlib: /home/ec2-user/environment/07_packaging/restaurant/src/front_of_house.rs /home/ec2-user/environment/07_packaging/restaurant/src/lib.rs
